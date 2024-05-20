6c9dee0faa498044e9ac33ffb3a9b46c4934607d
isaias_pruebas <isgb_codigo@hotmail.com>
2024-05-19 21:35:41 -0600

    debug

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+function useMyHook() {
+  const [active, setActive] = useState(true);
 
-  const [active, setActive] = useState(true)
-  const handleToggle = () => setActive(!active)
-  const handleTrue = () => setActive(true)
-  const handleFalse = () => setActive(false)
+  const handleToggle = () => setActive(!active);
+  const handleTrue = () => setActive(true);
+  const handleFalse = () => setActive(false);
+
+  return {
+    active,
+    handleToggle,
+    handleTrue,
+    handleFalse,
+  };
+}
+
+function ShowInfo() {
+  const { active, handleToggle } = useMyHook();
 
   return (
     <div>
-{/* https://www.youtube.com/watch?v=xutmKvLsbKM */}
-      <button onClick={handleToggle}>
-        Click
-      </button>
+      <div style={{ background: "#000", color: "white" }}>
+        <button onClick={() => handleToggle()}>Show/Hide</button>
 
-      <button onClick={handleTrue}>
-        True
-      </button>
+        {active && (
+          <div>
+            <h1>Show user!</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 
-      <button onClick={handleFalse}>
-        False
-      </button>
+function App() {
+  const { active, handleToggle, handleFalse } = useMyHook();
+
+  return (
+    <div>
+      {/* https://www.youtube.com/watch?v=xutmKvLsbKM */}
+      <button onClick={handleToggle}>Click</button>
+
+      <button onClick={handleTrue}>True</button>
+
+      <button onClick={handleFalse}>False</button>
 
       {active.toString()}
 
+      <ShowInfo />
     </div>
   );
 }
